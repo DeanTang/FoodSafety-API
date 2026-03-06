a445cc47fc5e551a825f664d27b234ad20c52936
DeanTang <30429736+DeanTang@users.noreply.github.com>
2026-03-06 17:32:54 +1100

    Added post and get operations

--- foodsafety-app/scalable-api-ts/src/routes/foods.ts
+++ foodsafety-app/scalable-api-ts/src/routes/foods.ts
@@ -4,4 +4,33 @@ import { food } from '../models/food';
 const router = Router();
 let foods: food[] = [];
 
+// post
+router.post('/', (req: Request, res: Response) => {
+    const food: food = {
+        id: foods.length + 1,
+        name: req.body.name,
+        fridgeLifeDays: req.body.fridgeLifeDays,
+        shelfLifeDays: req.body.shelfLifeDays
+    };
+
+    foods.push(food);
+    res.status(201).json(food);
+});
+
+//get
+router.get('/', (req: Request, res: Response) => {
+  res.json(foods);
+});
+
+//get one
+router.get('/:id', (req: Request, res: Response) => {
+  const food = foods.find((t) => t.id === parseInt(req.params.id));
+
+  if (!food) {
+    res.status(404).send('Food product not found');
+  } else {
+    res.json(food);
+  }
+});
+
 export default router;
