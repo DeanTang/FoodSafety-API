6cbc128e61b5bd21ca8d47e1ab072b00e45a39f0
DeanTang <30429736+DeanTang@users.noreply.github.com>
2026-03-20 23:07:11 +1100

    Added update and delete operations

--- foodsafety-app/scalable-api-ts/src/routes/foods.ts
+++ foodsafety-app/scalable-api-ts/src/routes/foods.ts
@@ -17,19 +17,47 @@ router.post('/', (req: Request, res: Response) => {
     res.status(201).json(food);
 });
 
-//get
+// get
 router.get('/', (req: Request, res: Response) => {
   res.json(foods);
 });
 
-//get one
+// get by id
 router.get('/:id', (req: Request, res: Response) => {
-  const food = foods.find((t) => t.id === parseInt(req.params.id));
+  const food = foods.find((f) => f.id === parseInt(req.params.id));
 
   if (!food) {
     res.status(404).send('Food product not found');
   } else {
     res.json(food);
+  }
+});
+
+// update
+router.put('/:id', (req: Request, res: Response) => {
+  const food = foods.find((f) => f.id === parseInt(req.params.id));
+
+  if (!food) {
+    res.status(404).send('Food not found');
+  } else {
+    food.name = req.body.name || food.name;
+    food.fridgeLifeDays = req.body.fridgeLifeDays || food.fridgeLifeDays;
+    food.shelfLifeDays = req.body.shelfLifeDays || food.shelfLifeDays;
+
+    res.json(food);
+  }
+});
+
+
+// delete
+router.delete('/:id', (req: Request, res: Response) => {
+  const index = foods.findIndex((f) => f.id === parseInt(req.params.id));
+
+  if (index === -1) {
+    res.status(404).send('Food not found');
+  } else {
+    foods.splice(index, 1);
+    res.status(204).send();
   }
 });
 
